e934ed2f250341a584f8194bebb8c3e0a59f0561
Github <mabdullaharis@gmail.com>
2019-03-31 15:26:12 +0530

    Modified.

---
+++ src/com/harism99/calculator/Addition.java
@@ -0,0 +1,10 @@
+package com.harism99.calculator;
+
+public class Addition {
+    public void add(int x, int y) {
+        int z;
+        z = x + y;
+        System.out.println("Addition of " + x + " and " + y + " is " + z);
+    }
+
+}
---
+++ src/com/harism99/calculator/Subtraction.java
@@ -0,0 +1,9 @@
+package com.harism99.calculator;
+
+public class Subtraction {
+    public void sub(int x, int y) {
+        int z;
+        z = x - y;
+        System.out.println("Subtraction of " + x + " and " + y + " is " + z);
+    }
+}
